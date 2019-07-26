e3a85b37151396e4313e6b424f46dd75463c9e59
Grayson Gantek <graysonleegantek@gmail.com>
2019-07-26 11:28:44 -0400

    Working search button, need to add Artist, album inputs

--- javascript.js
+++ javascript.js
@@ -1,45 +1,20 @@
 const apiKey = "d5de65c79f7e74381ecb55b7caad1609";
 
-$("#find-movie").on("click", displayLyrics);
-
-function displayLyrics(e) {
-    e.preventDefault();
+function displayLyrics() {
     
     const songName = $("#userInput").val();
-    const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
-    const musixUrl = `${corsAnywhere}https://api.musixmatch.com/ws/1.1/track.search?apikey=${apiKey}&q_track=${songName}`;
+    const corsAnywhere = "http://cors-anywhere.herokuapp.com/";
+    const musixUrl = `${corsAnywhere}http://api.musixmatch.com/ws/1.1/track.search?apikey=${apiKey}&q_track=${songName}`;
 
     $.ajax({
         url: musixUrl,
         method: "GET"
-    }).then(function (response) {
-        console.log(response.json());
-        const fetchedSongName = response.message.body;
+    }).then(function (responseString) {
+        let response = JSON.parse(responseString);
+        response = response.message.body;
+        console.log(response.track_list[0].track.track_name);
+        console.log(response.track_list[0].track.artist_name);
+        console.log(response.track_list[0].track.album_name);
+        // grabs first song's name
     });
 }
-
-// $.ajax({
-//     url: "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?apikey=d5de65c79f7e74381ecb55b7caad1609&q_track=test",
-//     method: "GET"
-// }).then(function (response) {
-//     console.log(response);
-// });
-
-// const apiKey = "d5de65c79f7e74381ecb55b7caad1609";
-
-// $("#find-movie").on("click", displayLyrics);
-
-// function displayLyrics(e) {
-//     console.log("Fetching lyrics...");
-//     e.preventDefault();
-
-//     let songName = $("#userInput").val();
-//     const musixUrl = `https://api.musixmatch.com/ws/1.1/track.search?apikey=${apiKey}&q_track=${songName}`;
-
-//     $.ajax({
-//         url: "https://api.musixmatch.com/ws/1.1/track.search?apikey=d5de65c79f7e74381ecb55b7caad1609&q_track=test",
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(JSNO.stringify(response));
-//     });
-// }
