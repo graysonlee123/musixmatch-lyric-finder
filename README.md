71460723cf80da7e6c2d39cf0f839bc920f14092
Grayson Gantek <graysonleegantek@gmail.com>
2019-07-26 12:34:13 -0400

    Switched over to general search and lyric pull is working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # musixmatch-lyric-finder
+
+https://developer.musixmatch.com/documentation
--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 const apiKey = "d5de65c79f7e74381ecb55b7caad1609";
+let trackID;
 
 function displayLyrics() {
     
-    const songName = $("#userInput").val();
+    const userSearch = $("#userInput").val();
     const corsAnywhere = "http://cors-anywhere.herokuapp.com/";
-    const musixUrl = `${corsAnywhere}http://api.musixmatch.com/ws/1.1/track.search?apikey=${apiKey}&q_track=${songName}`;
+    const musixUrl = `${corsAnywhere}http://api.musixmatch.com/ws/1.1/track.search?apikey=${apiKey}&q=${userSearch}&s_track_rating=desc`;
 
     $.ajax({
         url: musixUrl,
@@ -12,9 +13,21 @@ function displayLyrics() {
     }).then(function (responseString) {
         let response = JSON.parse(responseString);
         response = response.message.body;
-        console.log(response.track_list[0].track.track_name);
-        console.log(response.track_list[0].track.artist_name);
-        console.log(response.track_list[0].track.album_name);
+        trackID = response.track_list[0].track.track_id;
+        console.log("Track ID: " + trackID)
+        console.log(response);
+        console.log("Track name: " + response.track_list[0].track.track_name);
+        console.log("Artist name: " + response.track_list[0].track.artist_name);
+        console.log("Album name: " + response.track_list[0].track.album_name);
         // grabs first song's name
+    }).then(function() {
+        $.ajax({
+            url: `${corsAnywhere}http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=${apiKey}&track_id=${trackID}`,
+            method: "GET"
+        }).then(function (lyricResponseString) {
+            let lyricResponse = JSON.parse(lyricResponseString);
+            lyricResponse = lyricResponse.message.body.lyrics.lyrics_body;
+            console.log(lyricResponse);
+        });
     });
 }
